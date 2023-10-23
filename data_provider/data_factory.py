56d9a7533d62a91522a48cd25b6fc8ce621962c9
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2023-10-23 21:24:27 +0200

    include the ravenc dataset in the factory

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,4 +1,4 @@
-from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_Pred
+from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_Pred, Dataset_RAVEnc
 from torch.utils.data import DataLoader
 
 data_dict = {
@@ -7,45 +7,72 @@ data_dict = {
     'ETTm1': Dataset_ETT_minute,
     'ETTm2': Dataset_ETT_minute,
     'custom': Dataset_Custom,
+    'RAVEnc': Dataset_RAVEnc,
 }
 
 
 def data_provider(args, flag):
     Data = data_dict[args.data]
-    timeenc = 0 if args.embed != 'timeF' else 1
 
-    if flag == 'test':
-        shuffle_flag = False
-        drop_last = True
-        batch_size = args.batch_size
-        freq = args.freq
-    elif flag == 'pred':
-        shuffle_flag = False
-        drop_last = False
-        batch_size = 1
-        freq = args.detail_freq
-        Data = Dataset_Pred
+    # for our RAVE encoded datasets
+    if args.data.lower() == 'ravenc':
+        shuffle_flag = flag == 'train'
+        drop_last = flag == 'train'
+        batch_size = args.batch_size if flag == 'train' else 1
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            csv_path=args.csv_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len],
+            scale=args.scale,
+            all_in_memory=args.all_in_memory
+        )
+        print(flag, len(data_set))
+        data_loader = DataLoader(
+            data_set,
+            batch_size=batch_size,
+            shuffle=shuffle_flag,
+            num_workers=args.num_workers,
+            drop_last=drop_last)
+        return data_set, data_loader
+
+    # for everything else (their original datasets)
     else:
-        shuffle_flag = True
-        drop_last = True
-        batch_size = args.batch_size
-        freq = args.freq
+        timeenc = 0 if args.embed != 'timeF' else 1
 
-    data_set = Data(
-        root_path=args.root_path,
-        data_path=args.data_path,
-        flag=flag,
-        size=[args.seq_len, args.label_len, args.pred_len],
-        features=args.features,
-        target=args.target,
-        timeenc=timeenc,
-        freq=freq
-    )
-    print(flag, len(data_set))
-    data_loader = DataLoader(
-        data_set,
-        batch_size=batch_size,
-        shuffle=shuffle_flag,
-        num_workers=args.num_workers,
-        drop_last=drop_last)
-    return data_set, data_loader
+        if flag == 'test':
+            shuffle_flag = False
+            drop_last = True
+            batch_size = args.batch_size
+            freq = args.freq
+        elif flag == 'pred':
+            shuffle_flag = False
+            drop_last = False
+            batch_size = 1
+            freq = args.detail_freq
+            Data = Dataset_Pred
+        else:
+            shuffle_flag = True
+            drop_last = True
+            batch_size = args.batch_size
+            freq = args.freq
+
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len],
+            features=args.features,
+            target=args.target,
+            timeenc=timeenc,
+            freq=freq
+        )
+        print(flag, len(data_set))
+        data_loader = DataLoader(
+            data_set,
+            batch_size=batch_size,
+            shuffle=shuffle_flag,
+            num_workers=args.num_workers,
+            drop_last=drop_last)
+        return data_set, data_loader
