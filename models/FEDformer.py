e65ec5594b15cb8b3fae67e3edd381f946100826
Balint Laczko <balintlaczko01@gmail.com>
2023-10-30 14:23:09 +0100

    more docs, debug prints and replicating our setup for testing

--- models/FEDformer.py
+++ models/FEDformer.py
@@ -345,7 +345,9 @@ class Model_noif(nn.Module):
     def forward(self, x_enc, x_mark_enc=None, x_dec=None, x_mark_dec=None,
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
         """
-        The main forward call of FEDformer
+        The main forward call of FEDformer.
+        The encoder input is the batch_x sequence (with seq_len time steps and 8 features).
+        The decoder input is the labels part of the batch_y sequence (label_len long) extended with zeros for the predictions (pred_len long).
 
         Args:
             x_enc (torch.Tensor): The input sequence for the encoder. Shape: [B, T, C] where T = seq_len, C is the number of features
@@ -356,81 +358,113 @@ class Model_noif(nn.Module):
         Returns:
             torch.Tensor: The predictions for the decoder. Shape: [B, pred_len, C] (I hope)
         """
+
+        # print(f'Encoder input: x_enc.shape: {x_enc.shape}') # [B, T, C], t = seq_len
+        # print(f'Prediction length: self.pred_len: {self.pred_len}') # t = pred_len
         # decomp init
-        # take the means per feature, and repeat it for the prediction length
-        # print(f'x_enc.shape: {x_enc.shape}')
-        # print(f'self.pred_len: {self.pred_len}')
+        # take the mean feature vector across all time steps, and repeat it for the prediction length
         mean = torch.mean(x_enc, dim=1).unsqueeze(
             1).repeat(1, self.pred_len, 1)  # [B, T, C], t = pred_len
-        # this seems unused
-        # zeros = torch.zeros([x_dec.shape[0], self.pred_len,
-        #                     x_dec.shape[2]]).to(device)  # cuda()
-        seasonal_init, trend_init = self.decomp(
-            x_enc)  # [B, T, C], [B, T, C], t = seq_len
+        # print(f'mean.shape: {mean.shape}')
+        seasonal_init, trend_init = self.decomp(x_enc)  # [B, T, C], [B, T, C], t = seq_len
+        # print(f'seasonal_init.shape: {seasonal_init.shape}')
+        # print(f'trend_init.shape: {trend_init.shape}')
+        
         # decoder input
         # concatenate the last label_len values of the trend_init with the mean that is pred_len long
-        trend_init = torch.cat(
-            [trend_init[:, -self.label_len:, :], mean], dim=1)  # [B, T, C], t = label_len + pred_len
-        # add zeros like [B, T, C], t = pred_len to the end of the seasonal_init
+        trend_init = torch.cat([trend_init[:, -self.label_len:, :], mean], dim=1)  # [B, T, C], t = label_len + pred_len
+        # print(f'Labels from trend and the mean for preds: trend_init.shape: {trend_init.shape}')
+        # add zeros like [B, T, C], t = pred_len to the labels part (here: 2nd half) of seasonal_init
         seasonal_init = F.pad(
             seasonal_init[:, -self.label_len:, :], (0, 0, 0, self.pred_len))  # [B, T, C], t = label_len + pred_len
+        # print(f'Labels from seasonal and zeros for preds: seasonal_init.shape: {seasonal_init.shape}')
+
         # enc
-        enc_out = self.enc_embedding(x_enc)
-        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+        enc_out = self.enc_embedding(x_enc) # [B, T, d_model], d_model = 512, t = seq_len
+        # print(f'Embedded encoder input: enc_out.shape: {enc_out.shape}')
+        enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask) # [B, T, d_model], d_model = 512, t = seq_len AND len(attns) = 2
+        # print(f'Encoder output: enc_out.shape: {enc_out.shape}')
+        # print(f'len(attns): {len(attns)}') # len(attns) == e_layers == 2
+        # print(f'attns: {attns}') # None, because output_attention = False
+
         # dec
-        dec_out = self.dec_embedding(x_dec)
+        # print(f'Decoder input (labels from sequence and zeros for pred_len): x_dec.shape: {x_dec.shape}') # [B, T, C], t = label_len + pred_len
+        dec_out = self.dec_embedding(x_dec) # [B, T, d_model], d_model = 512, t = label_len + pred_len
+        # print(f'Embedded decoder input (labels from sequence and zeros for pred_len): dec_out.shape: {dec_out.shape}')
         seasonal_part, trend_part = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask,
                                                  trend=trend_init)
+        # print(f'Decoder out / seasonal: seasonal_part.shape: {seasonal_part.shape}')
+        # print(f'Decoder out / trend: trend_part.shape: {trend_part.shape}')
+
         # final
         dec_out = trend_part + seasonal_part
+        # print(f'Seasonal + trend: dec_out.shape: {dec_out.shape}')
+        final_out = dec_out[:, -self.pred_len:, :]
+        # print(f'Chopping off only predictions: final_out.shape: {final_out.shape}')
 
-        return dec_out[:, -self.pred_len:, :]
+        return final_out
 
 
 if __name__ == '__main__':
     class Configs(object):
-        ab = 0
-        modes = 64
+        # ab = 0
+        # version = 'Fourier'
         mode_select = 'random'
-        version = 'Fourier'
-        # version = 'Wavelets'
-        # moving_avg = [12, 24]
-        moving_avg = 6
-        L = 1
+        modes = 64
+        L = 3
         base = 'legendre'
         cross_activation = 'tanh'
-        # seq_len = 96
+
         seq_len = 32
-        # label_len = 48
         label_len = 16
-        # pred_len = 96
         pred_len = 8
-        output_attention = False
+
         enc_in = 8
         dec_in = 8
+        c_out = 8
         d_model = 512
-        # embed = 'timeF'
-        embed = 'token_only'
-        dropout = 0.05
-        freq = 'h'
-        factor = 1
         n_heads = 8
-        d_ff = 16
         e_layers = 2
         d_layers = 1
-        c_out = 8
+        d_ff = 2048
+        moving_avg = 6
+        dropout = 0.05
         activation = 'gelu'
-        wavelet = 0
+        output_attention = False
+
+        # embed = 'timeF'
+        # embed = 'token_only'
+        # freq = 'h'
+        # factor = 1
+        # wavelet = 0
+
+        batch_size = 512
 
     configs = Configs()
     model = Model_noif(configs)
 
     print('parameter number is {}'.format(sum(p.numel()
           for p in model.parameters())))
-    enc = torch.randn([3, configs.seq_len, 8])
-    # enc_mark = torch.randn([3, configs.seq_len, 4])
+    print()
 
-    dec = torch.randn([3, configs.seq_len//2+configs.pred_len, 8])
-    # dec_mark = torch.randn([3, configs.seq_len//2+configs.pred_len, 4])
-    out = model.forward(enc, None, dec, None)
-    print(out.shape)
+    # batch_x is the input sequence: seq_len time steps with 8 features
+    batch_x = torch.randn([configs.batch_size, configs.seq_len, 8]) # [B, T, C], t = seq_len
+    # batch_y is the label sequence: label_len + pred_len time steps with 8 features
+    batch_y = torch.randn([configs.batch_size, configs.label_len+configs.pred_len, 8]) # [B, T, C], t = label_len + pred_len
+
+    # decoder input
+    # create a tensor taking the labels and extending it with zeros for the predictions
+    # containter for the last pred_len time steps
+    dec_inp = torch.zeros_like(batch_y[:, -configs.pred_len:, :]).float() # [B, T, C], t = pred_len
+    # concatenate the first label_len time steps with the container (which are zeros for pred_len time steps)
+    dec_inp = torch.cat([batch_y[:, :configs.label_len, :], dec_inp], dim=1).float() # [B, T, C], t = label_len + pred_len
+
+    outputs = model(batch_x, None, dec_inp, None) # [B, T, C], t = pred_len
+
+    # slice out the predictions from the end of the labels
+    batch_y = batch_y[:, -configs.pred_len:, :] # [B, T, C], t = pred_len
+
+    # compare the predictions with the labels
+    loss = torch.nn.MSELoss()(outputs, batch_y)
+
+    print(f"loss (model predictions MSEd with the truth at prediction time steps from training data): {loss}")
