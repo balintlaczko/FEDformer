9ce860dccd59463d3020406a4c128a31ed898e5c
Balint Laczko <balintlaczko01@gmail.com>
2023-11-03 21:28:04 +0100

    cleaned up script for command line use

--- predict.py
+++ predict.py
@@ -7,6 +7,17 @@ import lightning.pytorch as pl
 from data_provider.data_factory import data_provider_ravenc
 from models import FEDformer
 import soundfile as sf
+import argparse
+import tqdm
+
+# %%
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_files', type=int, default=1, help='number of files to generate')
+parser.add_argument('--num_steps', type=int, default=100, help='number of embedding steps to generate per file')
+parser.add_argument('--output_folder', type=str, default='generated_audio', help='output folder to save generated audio')
+parser.add_argument('--device', type=str, default='cuda', help='device to use for inference (cpu or cuda). Default: cuda')
+cmd_args = parser.parse_args()
 
 # %%
 # basic config
@@ -59,132 +70,83 @@ val_dataset, val_loader = data_provider_ravenc(args, "val", scaler=train_set.sca
 test_dataset, test_loader = data_provider_ravenc(args, "test", scaler=train_set.scaler, train_set=train_set)
 
 # %%
-# convert configs to dict
-def props(cls):   
-  return [i for i in cls.__dict__.keys() if i[:1] != '_']
-
-properties = props(Configs)
-print(properties)
-
-dict_args = {}
-for prop in properties:
-    dict_args[prop] = getattr(args, prop)
-print(dict_args)
-
-# %%
-# load checkpoint
-checkpoint_path = "checkpoints/model_hpc-v1.ckpt"
-checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
-print(checkpoint.keys())
+# torch device
+device = torch.device(cmd_args.device)
 
 # %%
 # load model
 checkpoint_path = "checkpoints/model_hpc-v1.ckpt"
-checkpoint = torch.load(checkpoint_path, map_location=torch.device('cuda'))
-fedformer = FEDformer.LitFEDformer(args).cuda()
+checkpoint = torch.load(checkpoint_path, map_location=device)
+fedformer = FEDformer.LitFEDformer(args).to(device)
 fedformer.load_state_dict(checkpoint['state_dict'])
 # disable randomness, dropout, etc...
 fedformer.eval()
 
 # %%
-# get a random batch from the test set
-# choose random id
-id = np.random.randint(len(test_dataset))
-
-# get the batch
-x, y = test_dataset[id]
-x = x.unsqueeze(0)
-y = y.unsqueeze(0)
-
-print(id)
-print(x.shape)
-print(y.shape)
-
-# %%
-# predict with the model
-dec_inp = torch.zeros_like(y[:, -args.pred_len:, :]).float()
-dec_inp = torch.cat([y[:, :args.label_len, :], dec_inp], dim=1).float()
-print(x.shape)
-print(dec_inp.shape)
-with torch.no_grad():
-    y_hat = fedformer.model(x, None, dec_inp, None)
-y_labels_for_preds = y[:, -args.pred_len:, :]
-loss = torch.nn.functional.mse_loss(y_hat, y_labels_for_preds)
-print(y_hat.shape)
-print(loss)
-
-# %%
 # load a pretrained RAVE model via torch.script
 models_folder = "rave_pretrained_models"
-# model_names = ["percussion", "darbouka_onnx",
-#                "nasa", "VCTK", "vintage", "wheel"]
 chosen_model = "VCTK"
-# models = [torch.jit.load(os.path.join(models_folder, name + ".ts"))
-#           for name in model_names]
-rave_model = torch.jit.load(os.path.join(models_folder, chosen_model + ".ts"), map_location=torch.device('cuda'))
-# eval mode
-# for model in models:
-#     model.eval()
+rave_model = torch.jit.load(os.path.join(models_folder, chosen_model + ".ts"), map_location=device)
 rave_model.eval()
 
 
 # %%
 # autoregressive setup
-# choose random id
-id = np.random.randint(len(test_dataset))
-print(f'random seed id: {id}')
-dataset_id, _ = test_dataset.chunk_dataset[id]
-print(f'dataset id: {dataset_id}')
-generated_length = 200
-num_generations = generated_length // args.pred_len
-# get the batch
-x, y = test_dataset[id]
-x = x.unsqueeze(0).cuda() # (1, seq_len, 8)
-y = y.unsqueeze(0).cuda() # (1, label_len + pred_len, 8)
+output_folder = cmd_args.output_folder
+os.makedirs(output_folder, exist_ok=True)
 
-zeros4preds = torch.zeros_like(y[:, -args.pred_len:, :]).float().cuda() # (1, pred_len, 8)
-y_labels = y[:, :args.label_len, :].cuda() # (1, label_len, 8)
+progress_bar = tqdm.tqdm(range(cmd_args.num_files))
+progress_bar.set_description(f"Generating {cmd_args.num_files} files")
 
-generated = []
-with torch.no_grad():
-    for i in range(num_generations):
-        # create a new decoder input
-        dec_inp = torch.cat([y_labels, zeros4preds], dim=1).float() # (1, label_len + pred_len, 8)
-        # get model predictions
-        model_preds = fedformer.model(x, None, dec_inp, None) # (1, pred_len, 8)
-        # append the predictions to the generated list
-        generated.append(model_preds)
-        # print(model_preds.shape)
-        # shift x by pred_len and append the predictions
-        x = torch.cat([x[:, args.pred_len:, :], model_preds], dim=1) # (1, seq_len, 8)
-        y_labels = x[:, -args.label_len:, :] # (1, label_len, 8)
+for generation_id in progress_bar:
+    # choose random id
+    id = np.random.randint(len(test_dataset))
+    dataset_id, _ = test_dataset.chunk_dataset[id]
+    generated_length = cmd_args.num_steps
+    num_generations = generated_length // args.pred_len
+    # get the batch
+    x, y = test_dataset[id]
+    x = x.unsqueeze(0).to(device) # (1, seq_len, 8)
+    y = y.unsqueeze(0).to(device) # (1, label_len + pred_len, 8)
 
-# concatenate all the generated predictions
-generated = torch.cat(generated, dim=1) # (1, generated_length, 8)
-print(generated.shape)
+    zeros4preds = torch.zeros_like(y[:, -args.pred_len:, :]).float().to(device) # (1, pred_len, 8)
+    y_labels = y[:, :args.label_len, :].to(device) # (1, label_len, 8)
 
-# %%
-# use the train set scaler to inverse transform the generated predictions
-generated = generated.squeeze(0).cpu().numpy()
-generated = train_set.scaler.inverse_transform(generated)
-generated = torch.from_numpy(generated).unsqueeze(0).cuda()
+    generated = []
+    with torch.no_grad():
+        for i in range(num_generations):
+            # create a new decoder input
+            dec_inp = torch.cat([y_labels, zeros4preds], dim=1).float() # (1, label_len + pred_len, 8)
+            # get model predictions
+            model_preds = fedformer.model(x, None, dec_inp, None) # (1, pred_len, 8)
+            # append the predictions to the generated list
+            generated.append(model_preds)
+            # print(model_preds.shape)
+            # shift x by pred_len and append the predictions
+            x = torch.cat([x[:, args.pred_len:, :], model_preds], dim=1) # (1, seq_len, 8)
+            y_labels = x[:, -args.label_len:, :] # (1, label_len, 8)
 
+    # concatenate all the generated predictions
+    generated = torch.cat(generated, dim=1) # (1, generated_length, 8)
 
-# %%
-# decode the generated predictions
-with torch.no_grad():
-    # reshape generated to (1, 8, generated_length) and decode
-    decoded = rave_model.decode(generated.transpose(1, 2))
-print(decoded.shape)
+    # %%
+    # use the train set scaler to inverse transform the generated predictions
+    generated = generated.squeeze(0).cpu().numpy()
+    generated = train_set.scaler.inverse_transform(generated)
+    generated = torch.from_numpy(generated).unsqueeze(0).to(device)
 
-# %%
-# save decoded to wav file
-# reshape to channels-last, squeeze batch dimension
-buffer = decoded.transpose(1, 2).squeeze(0).cpu().numpy()
-print(buffer.shape)
-destination_folder = "generated_audio"
-os.makedirs(destination_folder, exist_ok=True)
-output_file_name = f"{chosen_model}_FEDformer_generated_.wav"
-# VCTK is RAVE V1 - Default, that uses 44100 Hz sampling rate
-sf.write(os.path.join(destination_folder, "generated.wav"), buffer, 44100)
+    # %%
+    # decode the generated predictions
+    with torch.no_grad():
+        # reshape generated to (1, 8, generated_length) and decode
+        decoded = rave_model.decode(generated.transpose(1, 2))
+
+    # %%
+    # save decoded to wav file
+    # reshape to channels-last, squeeze batch dimension
+    buffer = decoded.transpose(1, 2).squeeze(0).cpu().numpy()
+    output_file_name = f"{chosen_model}_FEDformer_generated_{generation_id}.wav"
+    # VCTK is RAVE V1 - Default, that uses 44100 Hz sampling rate
+    sf.write(os.path.join(output_folder, output_file_name), buffer, 44100)
+
 # %%
