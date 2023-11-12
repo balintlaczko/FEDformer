5efe7a80d345378bd3cb9f4ea0f9f64b7e9ff885
Balint Laczko <balintlaczko01@gmail.com>
2023-11-12 11:41:06 +0100

    additionally chunk concat embeddings
    to avoid having speakers only in train or val or test

--- data_provider/rave_vctk_ds_concat_subjects.py
+++ data_provider/rave_vctk_ds_concat_subjects.py
@@ -54,13 +54,6 @@ for subject_id in tqdm.tqdm(df["subject_id"].unique()):
     subjects_ids.append(subject_id)
 
 # %%
-for id, subject_id in enumerate(subjects_ids):
-    print(subject_id)
-    print(subjects_embeddings[id].shape)
-# %%
-len(subjects_embeddings)
-
-# %%
 # create a new h5 dataset with the concatenated embeddings
 new_db_path = "../data/RAVE_encoded_datasets/vctk_trimmed_rave_encoded_concat_subjects.h5"
 with h5py.File(new_db_path, 'w') as f:
@@ -82,4 +75,41 @@ subjects_concat_df = subjects_concat_df.reset_index(drop=True)
 # save the dataframe to a csv file
 path_to_csv = "../data/RAVE_encoded_datasets/vctk_trimmed_rave_encoded_concat_subjects.csv"
 subjects_concat_df.to_csv(path_to_csv, index=False)
+
+
+# %%
+# chunk each embedding into chunks
+num_chunks_per_subject = 32
+subjects_embeddings_chunked = [torch.chunk(subject_embedding, num_chunks_per_subject, dim=-1) for subject_embedding in subjects_embeddings]
+subject_ids_chunked = []
+flat_chunks = []
+for subject_id, subject_embeddings_chunked in zip(subjects_ids, subjects_embeddings_chunked):
+    for i, subject_embedding_chunk in enumerate(subject_embeddings_chunked):
+        subject_ids_chunked.append(subject_id)
+        flat_chunks.append(subject_embedding_chunk)
+
+
+# %%
+# create another h5 dataset with the chunked embeddings
+new_db_path = "../data/RAVE_encoded_datasets/vctk_trimmed_rave_encoded_concat_subjects_chunked.h5"
+with h5py.File(new_db_path, 'w') as f:
+    for id, chunk in enumerate(flat_chunks):
+        f.create_dataset(str(id), data=chunk.numpy())
+
+
+# %%
+# create another dataframe with the subject ids and the dataset indices
+chunked_df = pd.DataFrame(subject_ids_chunked, columns=["subject_id"])
+chunked_df["dataset_index"] = [i for i in range(len(flat_chunks))]
+chunked_df_train, chunked_df_test = train_test_split(chunked_df, test_size=0.2, random_state=42)
+chunked_df_train, chunked_df_val = train_test_split(chunked_df_train, test_size=0.2, random_state=42)
+# merge the 3 datasets into one
+chunked_df_train["dataset"] = "train"
+chunked_df_val["dataset"] = "val"
+chunked_df_test["dataset"] = "test"
+chunked_df = pd.concat([chunked_df_train, chunked_df_val, chunked_df_test], axis=0)
+chunked_df = chunked_df.reset_index(drop=True)
+# save the dataframe to a csv file
+path_to_csv = "../data/RAVE_encoded_datasets/vctk_trimmed_rave_encoded_concat_subjects_chunked.csv"
+chunked_df.to_csv(path_to_csv, index=False)
 # %%
